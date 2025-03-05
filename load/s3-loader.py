76f140f95724417302442fe293ed80b604d23dc5
iamsohaib7 <sohaibt884@gmail.com>
2025-03-05 16:31:58 +0500

    add: compression in parquet upload to s3

--- load/s3-loader.py
+++ load/s3-loader.py
@@ -11,13 +11,14 @@ logging.basicConfig(level=logging.INFO)
 def upload_df_to_s3(df: pd.DataFrame, bucket: str, key: str):
     """
     it uploads a dataframe to an S3 bucket in parquet format
+    :param key: str
     :param df: pd.DataFrame
-    :param bucket: str    :param key: str
+    :param bucket: str
     :return: True if file was uploaded, else False
     """
     logging.info(f"Uploading {key} to {bucket}")
     parquet_buffer = io.BytesIO()
-    df.to_parquet(parquet_buffer, engine="pyarrow")
+    df.to_parquet(parquet_buffer, engine="pyarrow", index=False, compression="snappy")
     try:
         s3 = boto3.resource("s3")
         s3.put_object(Bucket=bucket, Key=key, Body=parquet_buffer.getvalue())
