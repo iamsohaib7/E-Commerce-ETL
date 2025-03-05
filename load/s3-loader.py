da38e187a73b7dd472c71fca2c441309eb83dd35
iamsohaib7 <sohaibt884@gmail.com>
2025-03-05 15:38:21 +0500

    add function to upload dataframe to s3

---
+++ load/s3-loader.py
@@ -0,0 +1,28 @@
+import io
+import logging
+
+import boto3
+import pandas as pd
+from botocore.exceptions import ClientError
+
+logging.basicConfig(level=logging.INFO)
+
+
+def upload_df_to_s3(df: pd.DataFrame, bucket: str, key: str):
+    """
+    it uploads a dataframe to an S3 bucket in parquet format
+    :param df: pd.DataFrame
+    :param bucket: str    :param key: str
+    :return: True if file was uploaded, else False
+    """
+    logging.info(f"Uploading {key} to {bucket}")
+    parquet_buffer = io.BytesIO()
+    df.to_parquet(parquet_buffer, engine="pyarrow")
+    try:
+        s3 = boto3.resource("s3")
+        s3.put_object(Bucket=bucket, Key=key, Body=parquet_buffer.getvalue())
+        logging.info(f"Uploaded {key} to {bucket}")
+        return True
+    except ClientError as e:
+        logging.error(e)
+    return False
