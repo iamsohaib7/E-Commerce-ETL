b0349f4452bcfb45c42bc86af54f56ee854d675d
iamsohaib7 <sohaibt884@gmail.com>
2025-03-09 19:26:23 +0500

    add: function to load data in snowflake

--- Load/snowflake_loader.py
+++ Load/snowflake_loader.py
@@ -1,7 +1,9 @@
 import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import pandas as pd
 import logging
 logging.basicConfig(level=logging.INFO)
 
@@ -31,4 +33,26 @@ def create_tables_in_snowflake(query: str):
                 return True
             except snowflake.connector.errors.ProgrammingError as e:
                 logging.error(e)
+        return False
+
+def load_df_to_snowflake(table: str, df: pd.DataFrame):
+    with snowflake.connector.connect(
+            user=SNOWFLAKE_USER,
+            password=SNOWFLAKE_PASSWORD,
+            account=SNOWFLAKE_ACCOUNT,
+            warehouse=SNOWFLAKE_WAREHOUSE,
+            schema=SNOWFLAKE_SCHEMA,
+            database=SNOWFLAKE_DATABASE,
+    ) as ctx:
+        try:
+            for column in df.columns:
+                if column.endswith("_id") and df[column].dtype == "object":
+                    df[column] = df[column].astype(str)
+            cols = df.select_dtypes(include=["datetime64[ns]"]).columns
+            for col in cols:
+                df[col] = df[col].dt.tz_localize("utc")
+            write_pandas(df=df, table_name=table, conn=ctx, use_logical_type=True)
+            return True
+        except snowflake.connector.errors.ProgrammingError as e:
+            logging.error(e)
         return False
